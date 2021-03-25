0d7549a2cda43dcb2832633281801f75982ac5d0
JamieShin0201 <jamieshin1450@gmail.com>
2021-03-25 18:28:36 +0900

    초기 프로젝트를 설정하라
    프로젝트를 생성하고 필요한 의존성들을 추가하였습니다.

---
+++ src/main/java/com/solebysole/SolebysoleApplication.java
@@ -0,0 +1,13 @@
+package com.solebysole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolebysoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolebysoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solebysole/SolebysoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solebysole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolebysoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

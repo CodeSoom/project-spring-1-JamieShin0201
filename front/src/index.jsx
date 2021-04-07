a4e1966de251509004f65a41b7d44b74e5c67657
JamieShin0201 <jamieshin1450@gmail.com>
2021-04-07 15:43:21 +0900

    ImageUploader를 추가하라

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,15 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ImageUploder from './services/image_uploader';
+import ImageFileInput from './components/imageFileInput/ImageFileInput';
 
 require('@fortawesome/fontawesome-free/js/all.js');
+
+const imageUploader = new ImageUploder();
+const FileInput = (props) => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App FileInput={FileInput} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),

ec415e72ac813cead10886c2a4ad49fd7edf2041
JamieShin0201 <jamieshin1450@gmail.com>
2021-04-27 15:41:13 +0900

    장바구니 상품 수정 메소드 안의 불필요한 코드를 삭제하라
    장바구니 상품이 영속상태이기에 Dirty Checking으로 정보가 수정됩니다.

--- src/main/java/com/solebysole/cart/application/CartProductService.java
+++ src/main/java/com/solebysole/cart/application/CartProductService.java
@@ -81,7 +81,6 @@ public class CartProductService {
         CartProduct cartProduct = findCartProduct(id);
 
         cartProduct.changeCount(cartProductUpdateData.getCount());
-        cartProductRepository.save(cartProduct);
     }
 
     /**
